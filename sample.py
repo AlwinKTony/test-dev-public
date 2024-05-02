cbbc94bce1121887dcf959d49f14194aa0f9d117
Alwin <alwintony69@gmail.com>
2024-05-01 22:48:21 -0500

    updates app

--- sample.py
+++ sample.py
@@ -7,3 +7,14 @@ while num != 0:
     num //= 10
 
 print("Reversed Number: " + str(reversed_num))
+EFRAIN = '''
+%d bottles of beer on the wall,
+%d bottles of beer,
+take one down, pass it around,
+%d bottles of beer on the wall!
+'''
+bottles_of_beer = 9
+while bottles_of_beer > 1:
+    print (REFRAIN % (bottles_of_beer, bottles_of_beer,
+        bottles_of_beer - 1))
+    bottles_of_beer -= 1
